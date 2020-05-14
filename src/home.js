80270cb02e6dbb7c890e3e0ad6fb2934bfa92358
Faris <86.faris@gmail.com>
2020-05-14 13:32:19 +0400

    commit from bracnh homepageanimation

--- src/home.js
+++ src/home.js
@@ -8,33 +8,99 @@ gsap.registerPlugin();
 var app = {
     loadLogo: function(){
         var tl = new TimelineMax({delay:.5});
-        tl.from('.g-logo',0.5,{scaleY:0,transformOrigin:'center',ease:Linear.easeNone},"index")
-          .to('.g-logo',0.5,{width:70,ease:Linear.easeNone},"index+=0.6")     
-          .to('.g-reverse',0.3,{clip:"rect(0px 45px 45px 0px)",ease:Linear.easeNone},"-=0.3")   
-          .to('.g-logo', 0.4, {clip:"rect(0px 70px 70px 70px)",ease:Linear.easeNone})
-          .to('.g-yellow', 0.3, {clip:"rect(0px 45px 45px 0px)",ease:Linear.easeNone},'-=0.3')
-          .to('.no-g', 3.0,{autoAlpha:1,ease:Linear.easeNone},"-=0.2")
-          .from('.separatorline',.75,{width:0,ease:Linear.easeNone},"-=2.5")
-          .staggerFrom('.intro-contact-details p.add-copy',1,{y:10,autoAlpha:0,ease:Linear.easeNone},.5,"-=1.5")
-          .from('.social',0.5,{y:10,autoAlpha:0,ease:Linear.easeNone},"-=.5")
-          .from('.intro-contact-details p.intro-copy',0.5,{y:10,autoAlpha:0,onComplete:this.slideDown.bind(this),ease:Linear.easeNone},"-=.25");
+        tl.from(
+          ".g-logo",
+          0.5,
+          { scaleY: 0, transformOrigin: "center", ease: "power1.Out" },
+          "index"
+        )
+          .to(".g-logo", 0.5, { width: 70, ease: "power1.Out" }, "index+=0.6")
+          .to(
+            ".g-reverse",
+            0.3,
+            { clip: "rect(0px 45px 45px 0px)", ease: "power1.Out" },
+            "-=0.3"
+          )
+          .to(".g-logo", 0.4, {
+            clip: "rect(0px 70px 70px 70px)",
+            ease: "power1.Out",
+          })
+          .to(
+            ".g-yellow",
+            0.3,
+            { clip: "rect(0px 45px 45px 0px)", ease: "power1.Out" },
+            "-=0.3"
+          )
+          .to(".no-g", 3.0, { autoAlpha: 1, ease: Linear.easeNone }, "-=0.2")
+          .from(
+            ".separatorline",
+            0.75,
+            { width: 0, ease: "power1.Out" },
+            "-=2.5"
+          )
+          .staggerFrom(
+            ".intro-contact-details p.add-copy",
+            0.5,
+            { y: 10, autoAlpha: 0, ease: "power1.In" },
+            0.5,
+            "-=2.0"
+          )
+          .from(
+            ".social",
+            0.5,
+            { y: 10, autoAlpha: 0, ease: "power1.In" },
+            "-=1.0"
+          )
+          .from(
+            ".intro-contact-details p.intro-copy",
+            0.5,
+            {
+              y: 10,
+              autoAlpha: 0,
+              onComplete: this.slideDown.bind(this),
+              ease: "power3.In",
+            },
+            "-=0.75"
+          );
     },
     slideDown: function() {
         var that = common;
-       // var slideDist = this.findContentSlideDistance();
-       /* setTimeout(function()  { */
+      
             if(that.checkHasClass(elements.wrapper,elements.slidedUpContentClass))        
            {
             var tl = new TimelineMax();  
-              tl.to(elements.wrapper, 0.75, {y:-120, ease:Linear.easeNone},"index")            
-              .to([elements.introContent,'.landing-logo','.no-g'],0.5,{autoAlpha:0,ease:Linear.easeNone},"-=0.1")    
-              .to(elements.logo_menu_frontpage,0.5,{y:120,ease:Linear.easeNone},"-=0.75")          
-              .to(elements.footerContent,0.5,{autoAlpha:1,ease:Linear.easeNone,onComplete:menuAnim.alignMenu.bind(menuAnim)},"+=0.1")
+              tl.to(
+                elements.wrapper,
+                0.75,
+                { y: -120, ease: "power1.out" /*Linear.easeNone*/ },
+                "index"
+              )
+                .to(
+                  [elements.introContent, ".landing-logo", ".no-g"],
+                  0.5,
+                  { autoAlpha: 0, ease: "power1.out" },
+                  "-=0.1"
+                )
+                .to(
+                  elements.logo_menu_frontpage,
+                  0.5,
+                  { y: 120, ease: "power1.out" },
+                  "-=0.75"
+                )
+                .to(
+                  elements.footerContent,
+                  0.5,
+                  {
+                    autoAlpha: 1,
+                    ease: "power1.out",
+                    onComplete: menuAnim.alignMenu.bind(menuAnim),
+                  },
+                  "+=0.1"
+                );
               
            }
            
              
-         /*}, secs);*/
    },
     getVideoURL:function(videoSrc){
         var videoPaths = videoSrc.split('|');
