b9219678e7c837f74c1145613afeb6a410725589
Faris <86.faris@gmail.com>
2020-06-10 10:29:58 +0400

    heightissue2

--- src/home.js
+++ src/home.js
@@ -150,9 +150,9 @@ window.addEventListener("load", function () {
   if (landingLogo) app.loadLogo();
 
   if (window.innerWidth < 1000) {
-    document.querySelector(".intro-video").setAttribute("style", `height:${window.innerHeight - 30}px;`);
+    document.querySelector(".intro-video").setAttribute("style", `height:${window.innerHeight - 80}px;`);
     document
       .querySelector(".fullscreen-vide-wrap")
-      .setAttribute("style", `height:${window.innerHeight - 30}px;`);
+      .setAttribute("style", `height:${window.innerHeight - 80}px;`);
   }
 });
